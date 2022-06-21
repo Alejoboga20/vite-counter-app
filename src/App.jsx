ca3370e70bd94996bd0303e9031e4e12863e8668
Alejo Boga <alejoboga20@gmail.com>
2022-06-21 13:17:13 -0300

    Use useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,5 @@ import { CounterApp } from './CounterApp';
 //import { FirstApp } from './FirstApp';
 
 export const App = () => {
-	return <CounterApp value={0} />;
+	return <CounterApp initialCounter={0} />;
 };
--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({ value }) => {
+export const CounterApp = ({ initialCounter = 0 }) => {
+	const [counter, setCounter] = useState(initialCounter);
+
+	const handleOnClick = () => {
+		setCounter(counter + 1);
+	};
+
 	return (
 		<>
 			<h1>CounterApp</h1>
-			<h2>{value}</h2>
+			<h2>{counter}</h2>
+
+			<button onClick={handleOnClick}>+ 1</button>
 		</>
 	);
 };
 
 CounterApp.propTypes = {
-	value: PropTypes.number.isRequired,
+	initialCounter: PropTypes.number.isRequired,
 };
