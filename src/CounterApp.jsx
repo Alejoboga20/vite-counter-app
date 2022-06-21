c30b339cbfca4ad2232a0e607f51f7e59c8d8751
Alejo Boga <alejoboga20@gmail.com>
2022-06-21 13:24:52 -0300

    Add operations

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 export const CounterApp = ({ initialCounter = 0 }) => {
 	const [counter, setCounter] = useState(initialCounter);
 
-	const handleOnClick = () => {
-		setCounter(counter + 1);
-	};
+	const handleAdd = () => setCounter(counter + 1);
+	const handleSubstract = () => setCounter(counter - 1);
+
+	const handleReset = () => setCounter(initialCounter);
 
 	return (
 		<>
 			<h1>CounterApp</h1>
 			<h2>{counter}</h2>
 
-			<button onClick={handleOnClick}>+ 1</button>
+			<button onClick={handleAdd}>+ 1</button>
+			<button onClick={handleSubstract}>- 1</button>
+			<button onClick={handleReset}> Reset </button>
 		</>
 	);
 };
