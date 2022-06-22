5408a5e37ac9636eae5edc2ccea52a484e12a92b
Alejo Boga <alejoboga20@gmail.com>
2022-06-22 10:21:18 -0300

    Delete snapshot

--- __tests__/FirstApp.test.jsx
+++ __tests__/FirstApp.test.jsx
@@ -4,10 +4,10 @@ import { FirstApp } from '../src/FirstApp';
 const title = 'Test Title';
 
 describe('Test First App', () => {
-	test('should match snapshot', () => {
+	/* 	test('should match snapshot', () => {
 		const { container } = render(<FirstApp title={title} />);
 		expect(container).toMatchSnapshot();
-	});
+	}); */
 
 	test('should show title in h1', () => {
 		const { getByText } = render(<FirstApp title={title} />);
