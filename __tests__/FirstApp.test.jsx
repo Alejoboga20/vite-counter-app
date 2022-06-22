a7358885fefec7e76ae51f5810b44cc8c5af3519
Alejo Boga <alejoboga20@gmail.com>
2022-06-22 10:17:10 -0300

    First Tests

--- __tests__/FirstApp.test.jsx
+++ __tests__/FirstApp.test.jsx
@@ -1,9 +1,16 @@
 import { render } from '@testing-library/react';
-
 import { FirstApp } from '../src/FirstApp';
+
+const title = 'Test Title';
 
 describe('Test First App', () => {
 	test('should match snapshot', () => {
-		render(<FirstApp title='Test Title' />);
+		const { container } = render(<FirstApp title={title} />);
+		expect(container).toMatchSnapshot();
+	});
+
+	test('should show title in h1', () => {
+		const { getByText } = render(<FirstApp title={title} />);
+		expect(getByText(title)).toBeTruthy();
 	});
 });
